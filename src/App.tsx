cffd013d65cde0d7f4c7af4a70b8ba753875ec66
2Chaos <ace.wonder@qq.com>
2023-03-10 22:16:05 +0800

    安装到本地能被运行的版本

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./index.css";
 import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
 import { Button, Space } from "antd";
 import { Input } from "antd";
 import { List } from "antd";
@@ -7,63 +8,103 @@ import {
   PaginationAlign,
   PaginationPosition,
 } from "antd/es/pagination/Pagination";
-export class App extends React.Component {
-  render() {
-    function change() {
-      setTodo([...todo, state]);
-      updateState("");
-    }
-    const [todo, setTodo] = useState<Array<string>>([]);
-    const [state, updateState] = useState<string>("");
-    const [position, _] = useState<PaginationPosition>("bottom");
-    const [align, __] = useState<PaginationAlign>("end");
-    //Math.floor((total + pagesize - 1) / pagesize));
-    return (
-      <article>
-        <h1>Todo</h1>
-        <Space>
-          <Input
-            style={{ width: 1293 }}
-            placeholder="请输入"
-            value={state}
-            onChange={(e) => {
-              updateState(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                setTodo([...todo, state]);
-                updateState("");
-              }
-            }}
-          ></Input>
-          <Space wrap>
-            <Button
-              type="dashed"
-              onClick={() => {
-                change();
-              }}
-            >
-              Orz
-            </Button>
-          </Space>
-        </Space>
 
-        <List
-          style={{ marginTop: 24 }}
-          header={<div>Header</div>}
-          footer={<div>Footer</div>}
-          bordered
-          dataSource={todo}
-          renderItem={(item) => <List.Item>{item}</List.Item>}
-          pagination={{
-            position,
-            align,
-            defaultCurrent: 1,
-            total: todo.length,
-            pageSize: 5,
-          }}
-        />
-      </article>
-    );
+function App() {
+  function change() {
+    setTodo([...todo, state]);
+    updateState("");
   }
+  function onDelete(index: any) {
+    todo.splice(index, 1);
+    setTodo([...todo]);
+  }
+  function swapItem(index1: any, index2: any) {
+    var temp = todo[index1];
+    todo[index1] = todo[index2];
+    todo[index2] = temp;
+    setTodo([...todo]);
+  }
+
+  const [todo, setTodo] = useState<Array<string>>([]);
+  const [state, updateState] = useState<string>("");
+  const [clickPageNum, setClickPageNum] = useState<number>();
+  let total = 0;
+  const [todo1, setTode] = useState(() => {
+    const todoList = [];
+    for (let index = 0; index < 100; index++) {
+      total++;
+      todoList.push({
+        title: "title" + index,
+        desc: "desc" + index,
+      });
+    }
+    return todoList;
+  });
+  /*
+   */
+  let pagesize = 5;
+
+  const [pageNum, setPageNum] = useState<number>(() => {
+    let i = (total + pagesize - 1) / pagesize;
+    while (1) {
+      if (i % 1 === 0) {
+        return i;
+      } else {
+        total = total - 1;
+        i = (total + pagesize - 1) / pagesize;
+      }
+    }
+  });
+  const [position, setPosition] = useState<PaginationPosition>("bottom");
+  const [align, setAlign] = useState<PaginationAlign>("end");
+  //Math.floor((total + pagesize - 1) / pagesize));
+  return (
+    <article>
+      <h1>Todo</h1>
+      <Space>
+        <Input
+          style={{ width: 1293 }}
+          placeholder="请输入"
+          value={state}
+          onChange={(e) => {
+            updateState(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              setTodo([...todo, state]);
+              updateState("");
+            }
+          }}
+        ></Input>
+        <Space wrap>
+          <Button
+            type="dashed"
+            onClick={() => {
+              change();
+            }}
+          >
+            Orz
+          </Button>
+        </Space>
+      </Space>
+
+      <List
+        style={{ marginTop: 24 }}
+        header={<div>Header</div>}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={todo}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+        pagination={{
+          position,
+          align,
+          defaultCurrent: 1,
+          total: todo.length,
+          pageSize: 5,
+        }}
+      />
+    </article>
+  );
 }
+
+export default App;
