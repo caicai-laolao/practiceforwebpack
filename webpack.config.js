9cc73ce9422f18aa3ecb4714347f242db00786f8
2Chaos <ace.wonder@qq.com>
2023-04-04 23:26:06 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    index: "./lib/index.js",
+    index: "./src/index.js",
   },
   devtool: "inline-source-map",
   devServer: {
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./lib/index.html",
+      template: "./src/index.html",
     }),
   ],
   output: {
