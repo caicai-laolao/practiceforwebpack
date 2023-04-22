e72f8f618a7cedfc045ba1dd55f7139d90a1f22d
2Chaos <ace.wonder@qq.com>
2023-04-22 10:13:21 +0800

    修改了文件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var module1 = require("./App.tsx");
-var module2 = require("./RecipeTitle.jsx");
+var module1 = require("./App.js");
+var module2 = require("./RecipeTitle.js");
 module.exports = {
   module1: module1,
-  module2: module2,
+  module2: module2
 };
---
+++ src/jst.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var Text = function Text() {
+  return /*#__PURE__*/_react["default"].createElement("text", null, "hello");
+};
+var _default = Text;
+exports["default"] = _default;
